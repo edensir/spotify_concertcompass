a282eb02231e892d6dc8ab5781c8acc09cbc21d8
Eden <e.siregar@live.nl>
2023-04-20 12:55:10 +0200

    database succesfully connected

---
+++ server/db.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.DB}`);
+        console.log('connected to database successfully');
+    } catch (error) {
+        console.log('could not connect to database: ', error);
+    }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './db';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+connectDB()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
